5ffb547aabeabae5b8ad5b980d1c082890aae15a
kpeng <294336370@qq.com>
2017-11-03 15:19:00 +0800

    机器学习 && 动画

---
+++ iOS_Animations_by_Tutorials_v3.1/14-gradient-animations/starter/SlideToReveal/AnimatedMaskLabel.swift
@@ -0,0 +1,67 @@
+/*
+* Copyright (c) 2014-2016 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import QuartzCore
+
+@IBDesignable
+class AnimatedMaskLabel: UIView {
+  
+  let gradientLayer: CAGradientLayer = {
+    let gradientLayer = CAGradientLayer()
+    
+    // Configure the gradient here
+    gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+    gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+    
+    let colors = [
+        UIColor.black.cgColor,
+        UIColor.white.cgColor,
+        UIColor.black.cgColor
+    ]
+    gradientLayer.colors = colors
+    
+    let locations: [NSNumber] = [
+        0.25,
+        0.5,
+        0.75 ]
+    gradientLayer.locations = locations
+    
+    return gradientLayer
+  }()
+  
+  @IBInspectable var text: String! {
+    didSet {
+      setNeedsDisplay()
+    }
+  }
+  
+  override func layoutSubviews() {
+    layer.borderColor = UIColor.green.cgColor
+  }
+  
+  override func didMoveToWindow() {
+    super.didMoveToWindow()
+    
+  }
+  
+}
